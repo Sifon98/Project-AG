0c4b9d820cb3139d91356d6a56ae26a52aff869a
Alexander Gustafsson <alegus753@gmail.com>
2023-10-04 15:05:52 +0200

    gh-pages fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/Sifon98.github.io',
+  base: "/Project-AG",
   server: {
     port:3000,
   }
