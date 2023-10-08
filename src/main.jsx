fc030dbdfbcaae6b920cefa1bc48d1f830257429
Alexander Gustafsson <alegus753@gmail.com>
2023-10-09 00:15:43 +0200

    changed to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter, } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* For GitHub Pages, safe to remove after */}
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/Project-AG/'}>
+    <HashRouter basename={import.meta.env.DEV ? '/' : '/Project-AG/'}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
