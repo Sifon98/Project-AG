9b1ad0b6334e9f833260b9ef29c836a350151114
Alexander Gustafsson <alegus753@gmail.com>
2023-10-08 18:21:01 +0200

    css fixes and array fixing

--- src/components/GuideArray.jsx
+++ src/components/GuideArray.jsx
@@ -5,11 +5,129 @@ export const preGuideArray = [
         guideTitle: "ROOT OF NIGHTMARES",
         guideUnderTitle: "All encounters • Challenges • Red border • Secret chests",
         guideText: "As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.",
+        guidePage: {
+            navigation : [
+                {
+                    href: "#intro",
+                    class:  "nav-large",
+                    text: "INTRO"
+                },
+                {
+                    href: "#intro",
+                    class:  "nav-small",
+                    text: "Red Border #1"
+                },
+                {
+                    href: "#cataclysm",
+                    class:  "nav-large",
+                    text: "CATACLYSM"
+                },
+                {
+                    href: "#cataclysm",
+                    class:  "nav-small",
+                    text: "Secret Chest #1"
+                },
+                {
+                    href: "#scission",
+                    class:  "nav-large",
+                    text: "SCISSION"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-large",
+                    text: "JUMPING PUZZLE"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-small",
+                    text: "Red Border #2"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-small",
+                    text: "Secret Chest #2"
+                },
+                {
+                    href: "#macrocosm",
+                    class:  "nav-large",
+                    text: "MACROCOSM"
+                },
+                {
+                    href: "#macrocosm",
+                    class:  "nav-small",
+                    text: "Red Border #3"
+                },
+                {
+                    href: "#nezarec",
+                    class:  "nav-large",
+                    text: "NEZAREC"
+                }
+            ],
+        }
     },
     // Guide 2 =========================================================================================================
     {guideId:"kingsfall", tag: "RAIDS",
         guideTitle: "KING'S FALL",
         guideUnderTitle: "All encounters • Challenges • Red border • Secret chests",
         guideText: "With one of the best boss entrances, King’s fall is one of the most iconic Raids in Destiny 2. This Raid makes sure that all members of your fireteam have a job to do which makes it very engaging. Some people might dislike the jumping puzzles but for some more sinister guardians, they are the highlight.",
+        guidePage: {
+            navigation : [
+                {
+                    href: "#intro",
+                    class:  "nav-large",
+                    text: "INTRO"
+                },
+                {
+                    href: "#intro",
+                    class:  "nav-small",
+                    text: "Red Border #1"
+                },
+                {
+                    href: "#cataclysm",
+                    class:  "nav-large",
+                    text: "CATACLYSM"
+                },
+                {
+                    href: "#cataclysm",
+                    class:  "nav-small",
+                    text: "Secret Chest #1"
+                },
+                {
+                    href: "#scission",
+                    class:  "nav-large",
+                    text: "SCISSION"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-large",
+                    text: "JUMPING PUZZLE"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-small",
+                    text: "Red Border #2"
+                },
+                {
+                    href: "#jumpingpuzzle",
+                    class:  "nav-small",
+                    text: "Secret Chest #2"
+                },
+                {
+                    href: "#macrocosm",
+                    class:  "nav-large",
+                    text: "MACROCOSM"
+                },
+                {
+                    href: "#macrocosm",
+                    class:  "nav-small",
+                    text: "Red Border #3"
+                },
+                {
+                    href: "#nezarec",
+                    class:  "nav-large",
+                    text: "ORYX"
+                }
+            ],
+        }
     },
 ]
--- src/pages/GuidePage.jsx
+++ src/pages/GuidePage.jsx
@@ -1,38 +1,124 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom"
 import { preGuideArray } from '../components/GuideArray'
+import { useSearchParams } from "react-router-dom"
+import TempImg from '../img/background.png'
 
 export function GuidePage() {
+    // Navigation Array
+    const [navArray, setNavArray] = useState();
+    // Get the array with all the guide info
+    const [queryParameters] = useSearchParams()
+    const urlGuideId = queryParameters.get('name')
+
+    preGuideArray.map((data) => (
+        console.log(data.guidePage.navigation)
+    ))
+
   return (
     <div id="intro" className="guide-page">
         <div className="guide-page-container">
             <div className="navigation-placeholder"/>
             <div className="navigation">
-                <h3>NAVIGATION</h3>
+                <h3>THE NAVIGATOR</h3>
                 <hr className="largeLine" />
                 <hr className="smallLine" />
-                <a href="#intro" className="nav-large">INTRO</a><br/>
+                { preGuideArray.map((data, i) => (
+                    data.guideId == urlGuideId ?
+                    data.guidePage.navigation.map((data, i) => (
+                        <><a href={data.href} className={data.class}>{data.text}</a><br/></>
+                    )) : null
+                ))}
+                {/* <a href="#intro" className="nav-large">INTRO</a>
                 <a href="#intro" className="nav-small">Red Border #1</a>
-                <a href="#intro" className="nav-large">CATACLYSM</a>
-                <a href="#intro" className="nav-small">Secret Chest #1</a>
-                <a href="#intro" className="nav-large">SCISSION</a>
-                <a href="#intro" className="nav-large">JUMPING PUZZLE</a>
-                <a href="#intro" className="nav-small">Red Border #2</a>
-                <a href="#intro" className="nav-small">Secret Chest #2</a>
-                <a href="#intro" className="nav-large">MACROCOSM</a>
-                <a href="#intro" className="nav-small">Red Border #3</a>
-                <a href="#intro" className="nav-large">NEZAREC</a>
+                <a href="#cataclysm" className="nav-large">CATACLYSM</a>
+                <a href="#cataclysm" className="nav-small">Secret Chest #1</a>
+                <a href="#scission" className="nav-large">SCISSION</a>
+                <a href="#jumpingpuzzle" className="nav-large">JUMPING PUZZLE</a>
+                <a href="#jumpingpuzzle" className="nav-small">Red Border #2</a>
+                <a href="#jumpingpuzzle" className="nav-small">Secret Chest #2</a>
+                <a href="#macrocosm" className="nav-large">MACROCOSM</a>
+                <a href="#macrocosm" className="nav-small">Red Border #3</a>
+                <a href="#nezarec" className="nav-large">NEZAREC</a> */}
             </div>
             <div className="information">
                 <section>
-                    <h3 className="medium-header">INTRO - ROOT OF NIGHTMARES</h3>
-                    <hr className="largeLine" />
-                    <hr className="smallLine" />
-                    <div className="info">
-                        <h3>GENERAL INFO</h3>
+                    <div className="wrapper">
+                        <h3 className="medium-header">INTRO - ROOT OF NIGHTMARES</h3>
+                        <hr className="largeLine" />
                         <hr className="smallLine" />
-                        <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
                     </div>
+                    <div id="cataclysm" className="fake-block"/>
+                </section>
+                <section>
+                    <div className="wrapper">
+                        <h3 className="medium-header">CATACLYSM</h3>
+                        <hr className="largeLine" />
+                        <hr className="smallLine" />
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
+                    </div>
+                    <div id="scission" className="fake-block"/>
+                </section>
+                <section>
+                    <div className="wrapper">
+                        <h3 className="medium-header">SCISSION</h3>
+                        <hr className="largeLine" />
+                        <hr className="smallLine" />
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
+                    </div>
+                    <div id="jumpingpuzzle" className="fake-block"/>
+                </section>
+                <section>
+                    <div className="wrapper">
+                        <h3 className="medium-header">JUMPING PUZZLE</h3>
+                        <hr className="largeLine" />
+                        <hr className="smallLine" />
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
+                    </div>
+                    <div id="macrocosm" className="fake-block"/>
+                </section>
+                <section>
+                    <div className="wrapper">
+                        <h3 className="medium-header">MACROCOSM</h3>
+                        <hr className="largeLine" />
+                        <hr className="smallLine" />
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
+                    </div>
+                    <div id="nezarec" className="fake-block"/>
+                </section>
+                <section>
+                    <div className="wrapper">
+                        <h3 className="medium-header">NEZAREC</h3>
+                        <hr className="largeLine" />
+                        <hr className="smallLine" />
+                        <div className="info">
+                            <h3>GENERAL INFO</h3>
+                            <hr className="smallLine" />
+                            <p>As one of the easier Raids in Destiny 2, Root of Nightmares tends to be more relaxing than other Raids. The large amount of viable movement techniques makes this a super enjoyable experience for players willing to learn more advanced movement tech.</p>
+                        </div>
+                    </div>
+                    <div className="fake-block"/>
                 </section>
             </div>
         </div>
